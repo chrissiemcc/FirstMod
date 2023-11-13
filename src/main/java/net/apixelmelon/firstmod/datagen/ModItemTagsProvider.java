0508880d10090429dc45a95f915b269296e436dc
Chris McCambridge <chrissiemcc44@gmail.com>
2023-11-13 22:29:29 +0000

    Added Sapphire armour and trimmable variants

--- src/main/java/net/apixelmelon/firstmod/datagen/ModItemTagsProvider.java
+++ src/main/java/net/apixelmelon/firstmod/datagen/ModItemTagsProvider.java
@@ -1,9 +1,11 @@
 package net.apixelmelon.firstmod.datagen;
 
 import net.apixelmelon.firstmod.FirstMod;
+import net.apixelmelon.firstmod.item.ModItems;
 import net.minecraft.core.HolderLookup;
 import net.minecraft.data.PackOutput;
 import net.minecraft.data.tags.ItemTagsProvider;
+import net.minecraft.tags.ItemTags;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.data.ExistingFileHelper;
 import org.jetbrains.annotations.Nullable;
@@ -18,6 +20,10 @@ public class ModItemTagsProvider extends ItemTagsProvider {
 
     @Override
     protected void addTags(HolderLookup.Provider pProvider) {
-
+        this.tag(ItemTags.TRIMMABLE_ARMOR)
+                .add(ModItems.SAPPHIRE_HELMET.get(),
+                        ModItems.SAPPHIRE_CHESTPLATE.get(),
+                        ModItems.SAPPHIRE_LEGGINGS.get(),
+                        ModItems.SAPPHIRE_BOOTS.get());
     }
 }
