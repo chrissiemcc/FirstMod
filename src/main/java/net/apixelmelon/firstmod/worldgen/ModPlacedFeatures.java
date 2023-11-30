2b1c376c2ef73c13b60c64f8b12478b4441725af
Chris McCambridge <chrissiemcc44@gmail.com>
2023-11-30 20:58:36 +0000

    Added pine tree generation

--- src/main/java/net/apixelmelon/firstmod/worldgen/ModPlacedFeatures.java
+++ src/main/java/net/apixelmelon/firstmod/worldgen/ModPlacedFeatures.java
@@ -1,10 +1,13 @@
 package net.apixelmelon.firstmod.worldgen;
 
 import net.apixelmelon.firstmod.FirstMod;
+import net.apixelmelon.firstmod.block.ModBlocks;
 import net.minecraft.core.Holder;
 import net.minecraft.core.HolderGetter;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
+import net.minecraft.data.worldgen.placement.PlacementUtils;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.levelgen.VerticalAnchor;
@@ -19,6 +22,8 @@ public class ModPlacedFeatures {
     public static final ResourceKey<PlacedFeature> SAPPHIRE_ORE_PLACED_KEY = registerKey("sapphire_ore_placed");
     public static final ResourceKey<PlacedFeature> NETHER_SAPPHIRE_ORE_PLACED_KEY = registerKey("nether_sapphire_ore_placed");
     public static final ResourceKey<PlacedFeature> END_SAPPHIRE_ORE_PLACED_KEY = registerKey("end_sapphire_ore_placed");
+
+    public static final ResourceKey<PlacedFeature> PINE_PLACED_KEY = registerKey("pine_placed");
 
     public static void bootstrap(BootstapContext<PlacedFeature> context) {
         HolderGetter<ConfiguredFeature<?, ?>> configuredFeatures = context.lookup(Registries.CONFIGURED_FEATURE);
@@ -36,6 +41,13 @@ public class ModPlacedFeatures {
         //VerticalAnchor determines the y level the ore will spawn
         //.uniform is equal generation between the two y levels specified. This can be replaced with .triangle for generation to be
         //rare at either end but common in the centre of the two y levels.
+
+        register(context, PINE_PLACED_KEY, configuredFeatures.getOrThrow(ModConfiguredFeatures.PINE_KEY),
+                VegetationPlacements.treePlacement(PlacementUtils.countExtra(3, 0.1f, 2),
+                        ModBlocks.PINE_SAPLING.get()));
+        //1st number is how many trees will be planted
+        //with the chance (2nd number) of getting an extra few trees (3rd number)
+        //ModBlocks.(insert sapling).get() should be added at the end to ensure the tree doesn't grow where the sapling could not survive
     }
 
     private static ResourceKey<PlacedFeature> registerKey(String name) {
